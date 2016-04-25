ba361efd939fd43e494795864a3a38f5d83f66b1
Sergio Ivan Lopez Monzon <sergioivan154@gmail.com>
2016-04-24 21:55:21 -0500

    buscador

--- app/src/main/java/com/devworms/editorial/mango/fragments/MenuFragment.java
+++ app/src/main/java/com/devworms/editorial/mango/fragments/MenuFragment.java
@@ -74,6 +74,7 @@ public class MenuFragment extends Fragment {
         recyclerView.setLayoutManager(new LinearLayoutManager(container.getContext()));
 
 
+
         Toolbar toolbar = (Toolbar) getActivity().findViewById(R.id.toolbar);
         toolbar.setBackgroundColor(getResources().getColor(R.color.barraPincipal));
 
