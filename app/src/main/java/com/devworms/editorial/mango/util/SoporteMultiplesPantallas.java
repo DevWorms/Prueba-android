df47e960206196e329a159f48015937189f4ff4f
sergio ivan lopez monzon <sergioivan154@gmail.com>
2016-05-05 01:57:59 -0500

    soporte a tabletas

--- app/src/main/java/com/devworms/editorial/mango/util/SoporteMultiplesPantallas.java
+++ app/src/main/java/com/devworms/editorial/mango/util/SoporteMultiplesPantallas.java
@@ -9,14 +9,23 @@ public class SoporteMultiplesPantallas {
 
         Double d = 0.0;
         switch (width){
-            case 1440:case 1080:case 480:
+
+            // dispositivos moviles
+            case 1440:case 1080:case 480:case 1200:
                 d = height*0.4;
                 break;
-            case 768:case 320:
+            case 768:case 320: case 800:
                 d = height*.44;
                 break;
             case 720:
                 d = height*.38;
+                break;
+            // tabletas
+            case 600:  // 7 pulgadas
+                d = height*.41;
+                break;
+            case 1536:  // 8 pulgadas
+                d = height*.54;
                 break;
             default:
                 d = height*0.4;
